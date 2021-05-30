6540500efca64a9377886a5833274862c0db65d2
matintosh <matiasmartineeez@gmail.com>
2021-05-30 05:55:06 -0300

    Carousel Controllers component

---
+++ src/components/BlcCarousel/Controllers.js
@@ -0,0 +1,14 @@
+import { StyledButtonsContainer, StyledController } from "./Styles";
+
+export default function Controllers({ current, onChange }) {
+  return (
+    <StyledButtonsContainer>
+      <StyledController onClick={() => onChange(current - 1)}>
+        <img src="/images/arrows/left.png" />
+      </StyledController>
+      <StyledController onClick={() => onChange(current + 1)}>
+        <img src="/images/arrows/right.png" />
+      </StyledController>
+    </StyledButtonsContainer>
+  );
+}
