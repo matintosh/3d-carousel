d8c32a73a2a4117c02c9b5b5c748f15add7426da
matintosh <matiasmartineeez@gmail.com>
2021-05-30 11:06:54 -0300

    Some fixes in ship component

--- src/components/Ship/index.js
+++ src/components/Ship/index.js
@@ -11,35 +11,20 @@ import {
   StyledPencil,
 } from "./Styles";
 
-const defaults = {
-  active: false,
-  image: "/images/ship_1.png",
-  sign: "/images/empire.png",
-  title: "TIE Fighter Concept",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-};
+export default function Ship(props) {
+  const { setEditableShip } = useContext(ShipsAppContext);
 
-export default function Ship({
-  active = defaults.image,
-  image = defaults.image,
-  sign = defaults.sign,
-  title = defaults.title,
-  description = defaults.description,
-}) {
-  const { editShips } = useContext(ShipsAppContext);
-  
   return (
-    <StyledContainer active={active}>
-      <StyledShip active={active} src={image} />
-      <StyledSign src={`/images/${sign}.png`} />
+    <StyledContainer active={props.active}>
+      <StyledShip active={props.active} src={props.image} alt={props.img_alt} />
+      <StyledSign src={`/images/${props.sign}.png`} alt={props.sign} />
       <StyledDescription>
-        <StyledTitle>{title}</StyledTitle>
-        {description}
+        <StyledTitle>{props.name}</StyledTitle>
+        {props.description}
         <StyledShadow />
       </StyledDescription>
-      {active && (
-        <StyledButton onClick={editShips}>
+      {props.active && (
+        <StyledButton onClick={() => setEditableShip({ ...props })}>
           Edit
           <StyledPencil>✎</StyledPencil>
         </StyledButton>
